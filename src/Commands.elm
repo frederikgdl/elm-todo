ab63e4d34bb99230ccc71396a04ff535a95aea3b
frederikgdl <frederikgdl@gmail.com>
2018-05-23 20:24:26 +0200

    Add editing

--- src/Commands.elm
+++ src/Commands.elm
@@ -3,9 +3,15 @@ module Commands exposing (..)
 import Http
 import Json.Decode as Decode
 import Json.Decode.Pipeline exposing (decode, required)
+import Json.Encode as Encode
 import Msgs exposing (Msg)
 import Models exposing (PlayerId, Player)
 import RemoteData
+
+
+fetchPlayersUrl : String
+fetchPlayersUrl =
+    "http://localhost:4000/players"
 
 
 fetchPlayers : Cmd Msg
@@ -13,11 +19,6 @@ fetchPlayers =
     Http.get fetchPlayersUrl playersDecoder
         |> RemoteData.sendRequest
         |> Cmd.map Msgs.OnFetchPlayers
-
-
-fetchPlayersUrl : String
-fetchPlayersUrl =
-    "http://localhost:4000/players"
 
 
 playersDecoder : Decode.Decoder (List Player)
@@ -31,3 +32,39 @@ playerDecoder =
         |> required "id" Decode.string
         |> required "name" Decode.string
         |> required "level" Decode.int
+
+
+savePlayerUrl : PlayerId -> String
+savePlayerUrl playerId =
+    "http://localhost:4000/players/" ++ playerId
+
+
+savePlayerCmd : Player -> Cmd Msg
+savePlayerCmd player =
+    savePlayerRequest player
+        |> Http.send Msgs.OnPlayerSave
+
+
+savePlayerRequest : Player -> Http.Request Player
+savePlayerRequest player =
+    Http.request
+        { body = playerEncoder player |> Http.jsonBody
+        , expect = Http.expectJson playerDecoder
+        , headers = []
+        , method = "PATCH"
+        , timeout = Nothing
+        , url = savePlayerUrl player.id
+        , withCredentials = False
+        }
+
+
+playerEncoder : Player -> Encode.Value
+playerEncoder player =
+    let
+        attributes =
+            [ ( "id", Encode.string player.id )
+            , ( "name", Encode.string player.name )
+            , ( "level", Encode.int player.level )
+            ]
+    in
+        Encode.object attributes
